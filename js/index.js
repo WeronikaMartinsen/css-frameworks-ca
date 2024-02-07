d02c928afca5702d5038817aa398a9a21c27f0e7
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-02-07 12:38:19 +0100

    created and new branch and new folders for posts

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ import { registerFormListener } from "./handle/register.js";
 
 import { loginFormListener } from "./handle/login.js";
 
+import * as posts from "./api/post/index.js";
+
 const path = location.pathname;
 
 if (path === "/index.html") {
